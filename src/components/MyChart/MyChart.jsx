54b3f1a84fd13fbff41dd8a7225228d3665ff508
Adnan Latif <adnanlatif@Adnans-MacBook-Air.local>
2025-02-16 20:29:52 +0500

    S

--- src/components/MyChart/MyChart.jsx
+++ src/components/MyChart/MyChart.jsx
@@ -15,14 +15,13 @@ const MyChart = () => {
   const pointsRef = useRef([]); // Storing points in a ref to avoid re-renders
   const isTrendLine = useRef(false);
   const chartRef = useRef(null);
-  const originRef = useRef({origin_x:0,origin_y:0})
   const canvasInputListenerRef = useRef(null);
 
   const isDragging = useRef(false);
-  let prevX = useRef(0), prevY = useRef(0);
 
   const getpoints = (position) => {
-    pointsRef.current = [...pointsRef.current, { x: position.x, y: position.y }]
+    const myp = new VisualSeriesPoint(position.x,position.y);
+    pointsRef.current = [...pointsRef.current,myp]
   }
 
 
@@ -30,15 +29,15 @@ const MyChart = () => {
     for (let i = 0; i < pointsRef.current.length; i++) {
       // If this is not the last point, draw a line to the next point
 
-      const myp = new VisualSeriesPoint(pointsRef.current[i].x,pointsRef.current[i].y);
+      //const myp = new VisualSeriesPoint(pointsRef.current[i].x,pointsRef.current[i].y);
 
 
       if (i % 2 === 0 && i < pointsRef.current.length - 1) {
-        const myp1 = new VisualSeriesPoint(pointsRef.current[i+1].x,pointsRef.current[i+1].y);
+        //const myp1 = new VisualSeriesPoint(pointsRef.current[i+1].x,pointsRef.current[i+1].y);
 
         ctx.beginPath();
-        ctx.moveTo(myp.x(chartRef.current.scale), myp.y(chartRef.current.scale)); // Move to the current point
-        ctx.lineTo(myp1.x(chartRef.current.scale), myp1.y(chartRef.current.scale)); // Draw a line to the next point
+        ctx.moveTo(pointsRef.current[i].x(chartRef.current.scale), pointsRef.current[i].y(chartRef.current.scale)); // Move to the current point
+        ctx.lineTo(pointsRef.current[i+1].x(chartRef.current.scale), pointsRef.current[i+1].y(chartRef.current.scale)); // Draw a line to the next point
         ctx.strokeStyle = 'blue'; // Set the stroke color to blue (or any color you want)
         ctx.stroke(); // Actually draw the line
         ctx.closePath();
@@ -48,7 +47,7 @@ const MyChart = () => {
       // Draw a circle (dot) at the current point
       // const myp = new VisualSeriesPoint(pointsRef.current[i].x,pointsRef.current[i].y);
       //ctx.arc(pointsRef.current[i].x+originRef.current.origin_x, pointsRef.current[i].y+originRef.current.origin_y, 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
-      ctx.arc(myp.x(chartRef.current.scale), myp.y(chartRef.current.scale), 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
+      ctx.arc(pointsRef.current[i].x(chartRef.current.scale), pointsRef.current[i].y(chartRef.current.scale), 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
       ctx.fillStyle = 'red'; // Set the fill color to red
       ctx.fill(); // Fill the circle
       ctx.closePath(); // Close the path
@@ -86,11 +85,8 @@ const MyChart = () => {
     chartRef.current = chart;
     const candles = generateCandlesData();
     chart.setData({ candles });
-    //console.log({candles})
-    //chart.setChartType("line");
-    chart.setCrossToolVisible("none");
     chart.enableUserControls();
-    chart.setGridVisible(false);
+    chart.setGridVisible(true);
 
     const customDrawer = {
       draw() {
@@ -101,7 +97,7 @@ const MyChart = () => {
 
         // to get actual coordinates for canvas we need to use scaleModel,
         // since actual coordinates in pixels depends on current zoom level and viewport (scale)
-        console.log(startCandle.x)
+        //console.log(startCandle.x)
         const [x1, y1] = [startCandle.x(chart.scale), startCandle.y(chart.scale)];
         const [x2, y2] = [lastCandle.x(chart.scale), lastCandle.y(chart.scale)];
 
@@ -128,15 +124,6 @@ const MyChart = () => {
   }, [])
 
   React.useEffect(()=>{
-
-    
-
-      
-      // originRef.current = {
-      //   origin_x:canvasRef.current.width/2,
-      //   origin_y:canvasRef.current.height/2
-      // }
-
       const container = document.getElementById('chart_container');
       const myc1 = container.querySelector('[data-element="crossToolCanvas"]');
 
@@ -154,48 +141,17 @@ const MyChart = () => {
         console.log("clicked")
           if (isTrendLine.current) {
             let c_p = chartRef.current.canvasInputListener.getCurrentMousePoint();
-            console.log(c_p)
+            //console.log(chartRef.current.crosshair.observeCrossToolChanged().observed)
+            //console.log(c_p)
             console.log("on click")
             let ctx = contextRef.current;
             const rect = ctx.canvas.getBoundingClientRect(); // Get canvas position relative to viewport
-            const x = e.clientX - rect.left; // Mouse X relative to canvas
+            const scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+            const x = e.clientX - rect.left - scrollLeft; // Mouse X relative to canvas
             const y = e.clientY - rect.top;  // Mouse Y relative to canvas
-            getpoints(c_p);
+            getpoints({x,y});
           }
         });
-      
-
-    //   myc1.addEventListener("mousedown", (event) => {
-    //       isDragging.current = true;
-    //       prevX.current = event.offsetX;
-    //       prevY.current = event.offsetY;
-    //   });
-
-    //   myc1.addEventListener("mousemove", (event) => {
-    //     if (!isDragging.current) return;
-    
-    //     let currentX = event.offsetX;
-    //     let currentY = event.offsetY;
-
-    //     //this.origin_x = this.sketch.mouseX + (this.origin_x - this.sketch.pmouseX)
-    //     //this.origin_y = this.sketch.mouseY + (this.origin_y - this.sketch.pmouseY)
-
-    //     originRef.current.origin_x = currentX + (originRef.current.origin_x - prevX.current);
-    //     originRef.current.origin_y = currentY + (originRef.current.origin_y - prevY.current);
-        
-    //     // Update previous coordinates
-    //     prevX.current = currentX;
-    //     prevY.current = currentY;
-    // });
-    
-    // myc1.addEventListener("mouseup", () => {
-    //     isDragging.current = false;
-    // });
-    
-    // myc1.addEventListener("mouseleave", () => {
-    //     isDragging.current = false;
-    // });
-    
 
   },[])
 
