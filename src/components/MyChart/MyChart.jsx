61ab53438656b7b2971dedf28c8c3a2b66686fba
Adnan Latif <adnanlatif@Adnans-MacBook-Air.local>
2025-02-16 18:12:06 +0500

    changes

--- src/components/MyChart/MyChart.jsx
+++ src/components/MyChart/MyChart.jsx
@@ -3,9 +3,10 @@ import "./mychart.css";
 import { createChart } from "@devexperts/dxcharts-lite";
 import generateCandlesData from '@devexperts/dxcharts-lite/dist/chart/utils/candles-generator.utils';
 import { getDefaultConfig } from '@devexperts/dxcharts-lite/dist/chart/chart.config';
-//import { CanvasInputListenerComponent } from '@devexperts/dxcharts-lite/dist/chart/inputlisteners/canvas-input-listener.component';
-//import EventBus from '@devexperts/dxcharts-lite/dist/chart/events/event-bus';
+import { CanvasInputListenerComponent } from '@devexperts/dxcharts-lite/dist/chart/inputlisteners/canvas-input-listener.component';
+import EventBus from '@devexperts/dxcharts-lite/dist/chart/events/event-bus';
 import { CanvasBoundsContainer } from '@devexperts/dxcharts-lite/dist/chart/canvas/canvas-bounds-container';
+import { VisualSeriesPoint } from '@devexperts/dxcharts-lite/dist/chart/model/data-series.model';
 
 const MyChart = () => {
 
@@ -28,19 +29,26 @@ const MyChart = () => {
   const drawTrendLines = (ctx) => {
     for (let i = 0; i < pointsRef.current.length; i++) {
       // If this is not the last point, draw a line to the next point
+
+      const myp = new VisualSeriesPoint(pointsRef.current[i].x,pointsRef.current[i].y);
+
+
       if (i % 2 === 0 && i < pointsRef.current.length - 1) {
+        const myp1 = new VisualSeriesPoint(pointsRef.current[i+1].x,pointsRef.current[i+1].y);
+
         ctx.beginPath();
-        ctx.moveTo(pointsRef.current[i].x+originRef.current.origin_x, pointsRef.current[i].y+originRef.current.origin_y); // Move to the current point
-        ctx.lineTo(pointsRef.current[i + 1].x+originRef.current.origin_x, pointsRef.current[i + 1].y+originRef.current.origin_y); // Draw a line to the next point
+        ctx.moveTo(myp.x(chartRef.current.scale), myp.y(chartRef.current.scale)); // Move to the current point
+        ctx.lineTo(myp1.x(chartRef.current.scale), myp1.y(chartRef.current.scale)); // Draw a line to the next point
         ctx.strokeStyle = 'blue'; // Set the stroke color to blue (or any color you want)
         ctx.stroke(); // Actually draw the line
         ctx.closePath();
-        //drawLine(ctx,pointsRef.current[i].x+originRef.current.origin_x, pointsRef.current[i].y+originRef.current.origin_y,pointsRef.current[i + 1].x+originRef.current.origin_x, pointsRef.current[i + 1].y+originRef.current.origin_y,1)
       }
 
       ctx.beginPath(); // Start a new path
       // Draw a circle (dot) at the current point
-      ctx.arc(pointsRef.current[i].x+originRef.current.origin_x, pointsRef.current[i].y+originRef.current.origin_y, 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
+      // const myp = new VisualSeriesPoint(pointsRef.current[i].x,pointsRef.current[i].y);
+      //ctx.arc(pointsRef.current[i].x+originRef.current.origin_x, pointsRef.current[i].y+originRef.current.origin_y, 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
+      ctx.arc(myp.x(chartRef.current.scale), myp.y(chartRef.current.scale), 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
       ctx.fillStyle = 'red'; // Set the fill color to red
       ctx.fill(); // Fill the circle
       ctx.closePath(); // Close the path
@@ -93,6 +101,7 @@ const MyChart = () => {
 
         // to get actual coordinates for canvas we need to use scaleModel,
         // since actual coordinates in pixels depends on current zoom level and viewport (scale)
+        console.log(startCandle.x)
         const [x1, y1] = [startCandle.x(chart.scale), startCandle.y(chart.scale)];
         const [x2, y2] = [lastCandle.x(chart.scale), lastCandle.y(chart.scale)];
 
@@ -111,11 +120,7 @@ const MyChart = () => {
 
     //console.log(chart.hitTestCanvasModel.canvasInputListener)
   
-    //console.log(chart)
-
-
-
-
+    console.log(chart)
 
     const context = chart.dynamicObjectsCanvasModel.ctx;
     contextRef.current = context;
@@ -137,16 +142,25 @@ const MyChart = () => {
 
       console.log(myc1);
 
+      if(myc1){
+      const eventBus = new EventBus();
+      const canvasInputListener = new CanvasInputListenerComponent(eventBus, myc1);
+      canvasInputListenerRef.current = canvasInputListener;
+      canvasInputListener.doActivate();
+      }
+
 
       myc1.addEventListener("click", (e) => {
         console.log("clicked")
           if (isTrendLine.current) {
+            let c_p = chartRef.current.canvasInputListener.getCurrentMousePoint();
+            console.log(c_p)
             console.log("on click")
             let ctx = contextRef.current;
             const rect = ctx.canvas.getBoundingClientRect(); // Get canvas position relative to viewport
             const x = e.clientX - rect.left; // Mouse X relative to canvas
             const y = e.clientY - rect.top;  // Mouse Y relative to canvas
-            getpoints({x,y});
+            getpoints(c_p);
           }
         });
       
