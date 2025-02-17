74377d7fcf207f3759d867ef9aa6ae319f5951e8
Adnan Latif <adnanlatif@Adnans-MacBook-Air.local>
2025-02-17 21:17:39 +0500

    ok

--- src/components/MyChart/MyChart.jsx
+++ src/components/MyChart/MyChart.jsx
@@ -3,9 +3,8 @@ import "./mychart.css";
 import { createChart } from "@devexperts/dxcharts-lite";
 import generateCandlesData from '@devexperts/dxcharts-lite/dist/chart/utils/candles-generator.utils';
 import { getDefaultConfig } from '@devexperts/dxcharts-lite/dist/chart/chart.config';
-import { CanvasInputListenerComponent } from '@devexperts/dxcharts-lite/dist/chart/inputlisteners/canvas-input-listener.component';
-import EventBus from '@devexperts/dxcharts-lite/dist/chart/events/event-bus';
 import { VisualSeriesPoint } from '@devexperts/dxcharts-lite/dist/chart/model/data-series.model';
+import CustomCrosstoolDrawer from "./drawer";
 
 const MyChart = () => {
 
@@ -14,14 +13,15 @@ const MyChart = () => {
   const pointsRef = useRef([]); // Storing points in a ref to avoid re-renders
   const isTrendLine = useRef(false);
   const chartRef = useRef(null);
-  const canvasInputListenerRef = useRef(null);
+  const candlesRef = useRef(null);
+
+  const okref = useRef(null);
 
   const getpoints = (position) => {
-    const myp = new VisualSeriesPoint(position.x,position.y);
+    const myp = new VisualSeriesPoint(position.x, position.y);
     console.log(myp);
     pointsRef.current.push(myp);
   }
-
 
   const drawTrendLines = (ctx) => {
     for (let i = 0; i < pointsRef.current.length; i++) {
@@ -29,17 +29,17 @@ const MyChart = () => {
       if (i % 2 === 0 && i < pointsRef.current.length - 1) {
         ctx.beginPath();
         ctx.moveTo(pointsRef.current[i].x(chartRef.current.scale), pointsRef.current[i].y(chartRef.current.scale));
-        ctx.lineTo(pointsRef.current[i+1].x(chartRef.current.scale), pointsRef.current[i+1].y(chartRef.current.scale)); 
-        ctx.strokeStyle = 'blue'; 
-        ctx.stroke(); 
+        ctx.lineTo(pointsRef.current[i + 1].x(chartRef.current.scale), pointsRef.current[i + 1].y(chartRef.current.scale));
+        ctx.strokeStyle = 'blue';
+        ctx.stroke();
         ctx.closePath();
       }
 
-      ctx.beginPath(); 
+      ctx.beginPath();
       ctx.arc(pointsRef.current[i].x(chartRef.current.scale), pointsRef.current[i].y(chartRef.current.scale), 5, 0, 2 * Math.PI); // Draw a circle with radius 5 at x, y
-      ctx.fillStyle = 'red'; 
-      ctx.fill(); 
-      ctx.closePath(); 
+      ctx.fillStyle = 'red';
+      ctx.fill();
+      ctx.closePath();
     }
 
   }
@@ -48,18 +48,16 @@ const MyChart = () => {
     const container = document.getElementById('chart_container');
     const myc1 = container.querySelector('[data-element="crossToolCanvas"]');
     canvasRef.current = myc1;
-    
+
     const getdc = getDefaultConfig();
     getdc.rtl = true;
 
-    const chart = createChart(container,getdc);
+    const chart = createChart(container, getdc);
     chartRef.current = chart;
-    const candles = generateCandlesData();
-    chart.setData({ candles });
-    console.log({candles})
-    chart.enableUserControls();
+    const candles_temp = generateCandlesData();
+    candlesRef.current = candles_temp;
+    chart.setData({ candles: candles_temp });
     chart.setGridVisible(true);
-
 
     const customDrawer = {
       draw() {
@@ -84,64 +82,89 @@ const MyChart = () => {
 
     chart.drawingManager.addDrawerAfter(customDrawer, 'trendLineDrawer', 'DYNAMIC_OBJECTS');
 
+    chart.crossToolComponent.registerCrossToolTypeDrawer(
+      'cross-and-labels',
+      new CustomCrosstoolDrawer(
+        chart.config,
+        chart.bounds,
+        chart.chartModel,
+        chart.paneManager,
+      ),
+    );
+
+    okref.current = new CustomCrosstoolDrawer(
+      chart.config,
+      chart.bounds,
+      chart.chartModel,
+      chart.paneManager,
+    );
+
+
     const context = chart.dynamicObjectsCanvasModel.ctx;
     contextRef.current = context;
 
   }, [])
 
-  React.useEffect(()=>{
-      const container = document.getElementById('chart_container');
-      const myc1 = container.querySelector('[data-element="crossToolCanvas"]');
+  React.useEffect(() => {
+    const container = document.getElementById('chart_container');
+    const myc1 = container.querySelector('[data-element="crossToolCanvas"]');
 
-      const candles = generateCandlesData();
+    // Get min/max for `timestamp` and `close`
+    if (candlesRef.current) {
+      // let minTimestamp = Math.min(...candlesRef.current.map(item => item.timestamp));
+      // let maxTimestamp = Math.max(...candlesRef.current.map(item => item.timestamp));
+      // let minClose = Math.min(...candlesRef.current.map(item => item.close));
+      // let maxClose = Math.max(...candlesRef.current.map(item => item.close));
 
-
-      // Get min/max for `timestamp` and `close`
-    let minTimestamp = Math.min(...candles.map(item => item.timestamp));
-    let maxTimestamp = Math.max(...candles.map(item => item.timestamp));
-    let minClose = Math.min(...candles.map(item => item.close));
-    let maxClose = Math.max(...candles.map(item => item.close));
-
-    console.log(minTimestamp,maxTimestamp);
-    console.log(minClose,maxClose);
-
-      //console.log(myc1);
-
-      if(myc1){
-      const eventBus = new EventBus();
-      const canvasInputListener = new CanvasInputListenerComponent(eventBus, myc1);
-      canvasInputListenerRef.current = canvasInputListener;
-      canvasInputListener.doActivate();
-      }
-
+      // console.log(minTimestamp, maxTimestamp);
+      //console.log(minClose, maxClose);
 
       myc1.addEventListener("click", (e) => {
 
+        //console.log(chartRef.current.crossToolComponent.observeCrossToolChanged().observed)
+        //let tval = chartRef.current.crossToolComponent.observeCrossToolChanged().value;
+        //let tvv = parseFloat(tval.time.split(' ')[0]);
+
+
+        //const container = document.getElementById('chart_container');
+        //const myc1 = container.querySelector('[data-element="crossToolCanvas"]');
+        //console.log(myc1.width,myc1.height)
         // Calculate scale factors
-        let scaleX = (maxTimestamp - minTimestamp) / canvasRef.current.width;
-        let scaleY = (maxClose - minClose) / canvasRef.current.height;
+        //let scaleX = (maxTimestamp - minTimestamp) / myc1.width;
+        //let scaleY = (maxClose - minClose) / myc1.height;
+
+        //console.log(scaleX,scaleY)
 
         console.log("clicked")
-          if (isTrendLine.current) {
-            //let c_p = chartRef.current.canvasInputListener.getCurrentPoint();
-            //console.log(chartRef.current.crosshair.observeCrossToolChanged().observed)
-            //console.log(c_p)
-            //const timestamp = minTimestamp + (c_p.x * scaleX);
-            //const close = maxClose - (c_p.y * scaleY);  // Invert y-axis to match typical graph behavior
-            //console.log(`Timestamp: ${timestamp}, Close: ${close}`);
+        if (isTrendLine.current) {
+          //let c_p = chartRef.current.canvasInputListener.getCurrentPoint();
+          let yop = chartRef.current.chartModel.priceFromY(chartRef.current.crosshair.observeCrossToolChanged().value.y);
+          let xop = chartRef.current.chartModel.fromX(chartRef.current.crosshair.observeCrossToolChanged().value.x);
 
-            let ctx = contextRef.current;
-            const rect = ctx.canvas.getBoundingClientRect(); // Get canvas position relative to viewport
-            const scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
-            const x = e.clientX - rect.left - scrollLeft; // Mouse X relative to canvas
-            const y = e.clientY - rect.top;  // Mouse Y relative to canvas
-            // getpoints(c_p);
-            console.log({x,y})
-            getpoints({x,y})
-          }
-        });
+          console.log(yop)
+          console.log(xop)
 
-  },[])
+          //console.log(c_p)
+
+          // let ctx = contextRef.current;
+          // const rect = ctx.canvas.getBoundingClientRect(); // Get canvas position relative to viewport
+          // const scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+          // const x = e.clientX - rect.left - scrollLeft; // Mouse X relative to canvas
+          // const y = e.clientY - rect.top;  // Mouse Y relative to canvas
+
+          // let timestamp = minTimestamp + (x * scaleX);
+          // let close = maxClose - (y * scaleY);  // Invert y-axis to match typical graph behavior
+          // console.log(`Timestamp: ${timestamp}, Close: ${close}`);
+
+          // getpoints(c_p);
+          //console.log({ x, y })
+          getpoints({ x:xop, y:yop })
+
+        }
+      });
+    }
+
+  }, [])
 
   return (
     <>
